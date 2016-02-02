cdbe92aad381850537bb769d4b4033fcabaa670d
jpo <jpossandon@gmail.com>
2016-02-02 13:06:15 +0100

    new code to select images and to test possible experimental desgins following a flat hazard function

---
+++ code/experiment/E275_jo_imagesel.m
@@ -0,0 +1,45 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Selection of images for E275
+% Images are taken from NBP databases
+%
+% Jpo, 2.02.2016 Hamburg
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% cd ~/trabajo/E275/stimuli/new/                    % to get the images from the new or old set of images
+cd ~/trabajo/E275/stimuli/old/
+D                   = dir;
+target_siz          = [1080,1920];                  % size Samsung Syncmaster P2370, BPN lab old EEG room
+
+for f = 561:length(D)
+    im_old          = imread(D(f).name);
+    im_sz           = size(im_old);
+    
+    if im_sz(2)/im_sz(1)==16/9                      % image has the same ratio, so ww might only need to change the scale
+        
+        im          = imresize(im_old,target_siz);
+        
+    elseif im_sz(2)/im_sz(1)<16/9                   % ratio is smaller, resize to the correct horizontal dimension and then crop what is rest in the vertical
+        
+        im          = imresize(im_old,[im_sz(1)*target_siz(2)/im_sz(2),target_siz(2)]);
+        aux_sz      = size(im);
+        tocrop      = aux_sz(1)-target_siz(1);
+        im          = im(floor(tocrop/2)+1:end-ceil(tocrop/2),:,:);
+    else
+        display(sprintf('\nImage %s has a size of %dx%d and was not processed',im_sz(1),im_sz(2)))
+        continue
+    end
+    
+    fh = figure;
+    set(fh,'Position',[-1268 -223 1269 928])
+    subplot(1,2,1)
+    imshow(im_old)
+    
+    subplot(1,2,2)
+    imshow(im)
+    if input('To keep? (0-no,1-yes):')    
+        imwrite(im, ['~/trabajo/E275/stimuli/possible/',...
+                    strtok(D(f).name,'.'),'.jpg'],'jpeg')
+    end
+    close(fh)
+end
+        
---
+++ code/experiment/E275_jo_test_decay.m
@@ -0,0 +1,139 @@
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% E275
+
+trialdur    = 8;
+halflife    = trialdur/12;
+decay       = log(2)./halflife;         %lambda
+mean_life   = 1/decay;
+min_length  = 1.0;
+display(sprintf('\nCalculating estimates for:\n\n  Trial duration : %4.2f s\n  Half-life      : %4.2f s\n  Lambda         : %4.2f\n  Mean interval  : %4.2f s',trialdur,halflife,decay,mean_life))
+
+
+nsim        = 1000;
+ntrials     = 289;
+binwidth    = .5;
+tbins       = 0:binwidth:trialdur;
+display(sprintf('\nRunning %d simulations of %d trials each\n',nsim,ntrials))
+
+%% 
+nevents                     = zeros(1,nsim);        % number of events per sim
+nostim                      = zeros(1,nsim);        % number of trials without stimulation
+ intervals                   = zeros(1,nsim);        % times between trials
+% intervals2                   = [];
+ntimes                      = zeros(nsim,length(tbins));
+for n =1:nsim
+    times                   = [];                   % times of event occurence
+    interv                  = [];                   
+    for nT = 1:ntrials
+        rvals               = min_length+(-1./decay .* log(rand([1 10])));
+        localsum            = cumsum(rvals(1:end));
+        if localsum(1)<trialdur
+            nevents(1,n)    = nevents(1,n)+find(localsum<trialdur,1,'last');
+            times           = [times,localsum(1:find(localsum<trialdur,1,'last'))];
+%                 interv          = [interv,mean(rvals(1:find(localsum<trialdur,1,'last')))];      % we need to take the mean of each trial interval, otrhewise we are waiting them more than they should
+%                interv          = [interv,rvals(1:find(localsum<trialdur,1,'last')+1)]; need to select also the interval that goes further of the length of the trial, otherwise we are sampling selectively short intervals
+            interv  = [interv diff([0 localsum(1:find(localsum<trialdur,1,'last')) trialdur])];
+
+        else
+            nostim(1,n)     = nostim(1,n)+1;
+%                  interv          = [interv,rvals(1)];      % sampling of the intervals that are longer than trial duration
+                 interv          = [interv,trialdur];     % this is what would happen, but it will result in an stimate smaller than expectide time because sometime the new event would occur at a duration longer than trialdur
+        end
+    end
+    ntimes(n,:)             = histc(times,tbins);
+    intervals(n)            = mean(interv);
+ end
+
+%%
+figure
+set(gcf,'Position',[160 15 982 690])
+subplot(2,3,1)
+hold on
+plot(tbins(1:end-1)+binwidth/2,ntimes(:,1:end-1)./repmat(sum(ntimes(:,1:end-1),2),1,length(tbins)-1),'Color',[.7 .7 .7])
+plot(tbins(1:end-1)+binwidth/2,mean(ntimes(:,1:end-1)./repmat(sum(ntimes(:,1:end-1),2),1,length(tbins)-1)),'LineWidth',2,'Color',[0 0 0])
+title('Event occurence time')
+xlabel('(t)')
+
+subplot(2,3,2)
+hist(nevents,50)
+title('Total # stim')
+xlabel('(# stimulations per sim)')
+
+
+subplot(2,3,3)
+hist(nostim,50)
+title('# trials wo stim')
+xlabel('(#/sim)')
+
+
+subplot(2,3,4)
+hist(intervals,50)
+title('mean ISI per sim')
+xlabel('ISI (s)')
+
+subplot(2,3,5)
+axis off
+text(0,1,sprintf('# Simulations                   : %d',nsim),'Fontsize',12)
+text(0,.9,sprintf('# Trials/sim                     : %d',ntrials),'Fontsize',12)
+text(0,.8,sprintf('# Trials dur                      : %d sec',trialdur),'Fontsize',12)
+text(0,.7,sprintf('Total viewing time          : %d min',ceil(ntrials*trialdur/60)),'Fontsize',12)
+text(0,.6,sprintf('Half-life                            : %2.1f',halflife),'Fontsize',12)
+text(0,.5,sprintf('Min t after start/stim         : %2.2f',min_length),'Fontsize',12)
+
+subplot(2,3,6)
+axis off
+text(0,.9,sprintf('<# total events>              ~ %d',round(decay*trialdur*ntrials)),'Fontsize',12)
+text(0,.8,sprintf('<# triasl wo events>        ~ %d (%4.1f%%)',round(ntrials/(2^(trialdur/halflife))),100/(2^(trialdur/halflife))) ,'Fontsize',12)
+text(0,.7,sprintf('<time betw. events>        ~ %4.2f sec',halflife/log(2)),'Fontsize',12)
+
+text(0,.5,sprintf('Mean # events                ~ %d',round(mean(nevents))),'Fontsize',12)
+text(0,.4,sprintf('Mean # trials wo events  ~ %d (%4.1f%%)',round(mean(nostim)),round(mean(nostim))/ntrials*100),'Fontsize',12)
+text(0,.3,sprintf('Mean time betw. events   ~ %4.2f sec',mean(intervals)),'Fontsize',12)
+
+%%
+% % what about the convergence of the time between event
+samp = [];
+for s = 1:100   % number samples per time
+    for rep = 1:ntrials
+        rvals                       = (-1./decay .* log(rand([1 s])));
+        samp(s,rep)                 = mean(rvals);
+    end
+    s
+end
+figure
+hold on
+plot(1:s,mean(samp,2))
+plot(1:s,mean(samp,2)+std(samp,1,2)./sqrt(rep),'--')
+plot(1:s,mean(samp,2)-std(samp,1,2)./sqrt(rep),'--')
+
+%%
+% hazard(t)=first_failure(t)/no_failure(t)
+% Flat hazard occurs when the probability of an event to occur is the same
+% independently of the time is has happened since last event.
+% hazard(t)=const (lambda)
+% IN ORDER OF THIS TO WORK WELL AND NOT BEING BIASED IT NEED TO SAMPLE
+% PROPERLY THE EXPONENTIAL DISTRIBUTION, the upper limit of the binning
+% need to be several times the halflife
+halflife            = 1;
+decay               = log(2)./halflife;  
+
+samples             = 500000;
+bins                = 10000;
+timelimit           = 30*halflife;
+binwidth            = timelimit/bins;
+rvals               = (-1./decay .* log(rand([1 samples])));
+nbins               = 0:binwidth:timelimit;
+n                   = histc(rvals,nbins);
+pn                  = n(1:end-2)./(sum(n));
+% 
+
+figure
+hold all
+h(1)                = plot(nbins(1:end-2),pn);
+h(2)                = plot(nbins(1:end-2),1-cumsum(pn));
+h(3)                = plot(nbins(1:end-2),pn./binwidth./[1,(1-cumsum(pn(1:end-1)))]);
+h(4)                = vline(halflife);
+h(5)                = line([nbins(1) nbins(end-2)],[decay decay],'Color',[0 0 0]);
+axis([0  timelimit -decay 2/decay])
+
+legend(h,{'Failure','Survival','Hazard','Half-life','Lambda'})
